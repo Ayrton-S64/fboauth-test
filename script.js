0353aa0531b55b12bfd69afaa9f5028484c15dae
Ayrton Soto <ayrtonxd.123@gmail.com>
2023-09-24 22:37:07 -0500

    ya no se

--- script.js
+++ script.js
@@ -102,11 +102,12 @@ function makeMeRequest(){
   );
 }
 function makePageFeedBasicRequest(){
+  console.log(appAccessToken)
   const pageId = document.getElementById('txtPageNameFeed').value
   FB.api(
     `/${pageId}/feed`,
     'GET',
-    {"access_token":appAccessToken},
+    {access_token:appAccessToken},
     function(response) {
       console.log('-----{{makePageFeedBasicRequest}}-------')
         console.log(response);
@@ -115,11 +116,12 @@ function makePageFeedBasicRequest(){
 }
 
 function makePageMetadataBasicRequest(){
+  console.log(appAccessToken)
   const pageId = document.getElementById('txtPageNameMetadata').value
   FB.api(
     `/${pageId}`,
     'GET',
-    {"access_token":appAccessToken,"fields":"category,fan_count,is_community_page,link,name,rating_count,talking_about_count,were_here_count,instagram_accounts{id,username,follow_count,followed_by_count,media_count},about"},
+    {access_token:appAccessToken,"fields":"category,fan_count,is_community_page,link,name,rating_count,talking_about_count,were_here_count,instagram_accounts{id,username,follow_count,followed_by_count,media_count},about"},
     function(response) {
         // Insert your code here
     }
